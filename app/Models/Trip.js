68fd80b4c4dce20533f812488cafe64b736c5e2d
TaylorBruun <taylorbruun@gmail.com>
2022-05-27 12:31:08 -0600

    trip class

--- app/Models/Trip.js
+++ app/Models/Trip.js
@@ -4,9 +4,10 @@ import { generateId } from "../Utils/generateId.js";
 export class Trip{
     constructor(tripData){
         this.id = generateId()
-        this.name = tripData.nama
-
-
+        this.name = tripData.name
+        this.type = tripData.type
+        this.date = tripData.date
+        this.cost = tripData.cost
     }
 
 
